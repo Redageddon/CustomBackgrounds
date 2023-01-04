db785f49b9182388e36ce98e55f9696a44b11fb3
redageddon <therealrubiksmaster@gmail.com>
2023-01-03 16:10:01 -0800

    Added a helper class

---
+++ CustomBackgrounds/Helpers/AssetBundleHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace CustomBackgrounds.Helpers;
+
+public static class AssetBundleHelpers
+{
+    private static readonly AssetBundle AssetBundle;
+
+    static AssetBundleHelpers()
+    {
+        Stream customBgShader = Assembly.GetCallingAssembly().GetManifestResourceStream("CustomBackgrounds.Resources.CustomBG")!;
+        byte[] data = new byte[customBgShader.Length];
+        int _ = customBgShader.Read(data, 0, (int)customBgShader.Length);
+
+        AssetBundle = AssetBundle.LoadFromMemory(data);
+    }
+
+    internal static Material? GetMaterialFromAssetBundle() => AssetBundle.LoadAsset<Material>("_CustomBGMat");
+}
