e80c0e19382c9f82a1487ec4a592a16bb99a6fb9
redageddon <therealrubiksmaster@gmail.com>
2023-01-03 16:15:35 -0800

    Added the initial class for patchers in case i implement it

---
+++ CustomBackgrounds/HarmonyPatches/EnvironmentPatches.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace CustomBackgrounds.HarmonyPatches;
+
+// TODO: Patches for environment
+public class EnvironmentPatches
+{
+    private readonly Harmony instance = new(nameof(CustomBackgrounds));
+
+    public bool IsPatched { get; private set; }
+
+    internal void ApplyHarmonyPatches()
+    {
+        if (!this.IsPatched)
+        {
+            this.instance.PatchAll(Assembly.GetExecutingAssembly());
+            this.IsPatched = true;
+        }
+    }
+
+    internal void RemoveHarmonyPatches()
+    {
+        if (this.IsPatched)
+        {
+            this.instance.UnpatchSelf();
+            this.IsPatched = false;
+        }
+    }
+}
